a00c1ce3bc071d983a4b66a21fc099331cb0876e
Chris Gaber <cgaber2045@greenvilleschools.us>
2017-08-25 09:47:42 -0400

    Initial sharing of project

---
+++ Introduction.java
@@ -0,0 +1,28 @@
+
+/**
+ * This program tells a few things about me.
+ * 
+ * @author Chris Gaber
+ * @version August 22, 2017
+ */
+public class Introduction
+{
+    public static void main(String[] args)
+    {
+        // heere are some facts about me
+        System.out.println("My name is Chris Gaber");
+        System.out.println("I was born in Mauldin, SC.");
+        System.out.println("I go to J.L. Mann High School.");
+        System.out.println("My brother is 14 and goes to this school.");
+        System.out.println("I like to sleep.");
+        
+        //now store some numbers
+        int year = 2017;
+        int age = 17;
+        
+        // Takes the current year and subtracts your age, prints it.
+        System.out.println("year - age");
+        System.out.println(year-age);
+        System.out.println("I am " + age + " years old.");
+    }
+}
